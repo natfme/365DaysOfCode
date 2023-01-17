d8793c07cb94b36a86e1a9fffb5d5e037a74da2d
NatFMe <ing.nataliafrancomedina@gmail.com>
2023-01-16 22:09:47 -0500

    add cambios

--- Kotlin/day3.kt
+++ Kotlin/day3.kt
@@ -1,2 +1,16 @@
 // Day 2 - 100 Days of Code: Input & Variables
 //What language will you study now?
+
+fun main() {
+    // Don't be afraid of the code below! 
+    // In the future, you will definitely cope with it, but for now just ignore it.
+    var (a, b) = readLine()!!.split(" ").map { it.toInt() }
+
+    // Write only exchange actions here. Do not touch the lines above
+    val c = a
+    a = b
+    b = c
+
+    // Do not touch the lines below
+    print("$a $b")
+}
