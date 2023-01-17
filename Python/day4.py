f4cba46e263be14c359548202dbb0e486bb22d6f
NatFMe <ing.nataliafrancomedina@gmail.com>
2023-01-17 16:59:00 -0500

    add day4

---
+++ Python/day4.py
@@ -0,0 +1,22 @@
+#----------------------------------------------
+#Print in color!
+"""Write your own adventure story and make your project POP with some color!"""
+#---------------------------------------------
+
+print("--- Welcome to your adventure simulator. ---")
+print("""You'll be asked a bunch of questions then we'll make you up an amazing story with YOU as the star!""")
+print()
+"""name = input("Yout name: ")
+enemy = input("Your worst enemy's name: ")
+superPower = input("Your super power: ")
+namePlace = input("Where do you live?: ")
+nameFavFood = input("Your favourite food: ")
+print()
+print("Our story begins as our hero name approaches a foreboding castle in", namePlace)
+print("Suddenly a bolt of lightning striked the ground at the feet of", name)
+print("Our final battle begins! shouts the evil", enemy, "clearly missing the fact that", name, "has the power of", superPower, "which means they\'ll win quite easily")
+template = f"Hello {name}! Your ability to {superPower} will make sure you never have to look at {enemy} again. Go eat {nameFavFood} as you walk down the streets of {namePlace} and use {superPower} for good and not evil!"
+print(template) """
+
+#Sneaky Extra Skill
+print("Uh, oh, you've been given a", "\033[31m", "warning", "\033[0m", "for being a bad, bad person.")
