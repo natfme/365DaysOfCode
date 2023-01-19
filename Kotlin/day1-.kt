bebe464f3583538ea810f5c4e27bebb358d53412
NatFMe <ing.nataliafrancomedina@gmail.com>
2023-01-19 14:41:32 -0500

    add day 5

---
+++ Kotlin/day1-.kt
@@ -0,0 +1,11 @@
+//Day one - Hello, World!
+
+
+fun main(){  //fun main(args : Array<String>)
+    println("Hello, World!")
+    println("and how are you?")
+    println("xD")
+    println("Day1 challenge")
+    println("Kotlin is fun xD")
+    println(Yaaay!)
+}
